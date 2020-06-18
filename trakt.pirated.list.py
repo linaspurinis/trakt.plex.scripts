f4d67c9faf882c463c08f1cb36c4389373f5a5e1
Linas Purinis <linas.purinis@gmail.com>
2020-06-18 11:58:33 +0300

    changed pirated url

--- trakt.pirated.list.py
+++ trakt.pirated.list.py
@@ -20,7 +20,8 @@ except ImportError:
            'values and rename it to config.py'))
     sys.exit(1)
 
-FEED_URL = 'http://torrentfreak.com/category/dvdrip/feed/'
+#FEED_URL = 'http://torrentfreak.com/category/dvdrip/feed/'
+FEED_URL = 'https://torrentfreak.com/top-10-most-torrented-pirated-movies/'
 
 def get_pirated_ids():
     rss = requests.get(FEED_URL).text
@@ -50,7 +51,7 @@ def main():
     # update list description
     trakt.put_oauth_request('users/me/lists/{0}'.format(list_id), data={
         'description': 'Updated at ' + datetime.today().strftime('%Y-%m-%d') + 
-        '\r\n\r\n' + 'feed:https://torrentfreak.com/category/dvdrip/feed/' +
+        '\r\n\r\n' + FEED_URL +
         '\r\n' + 'Source code: https://github.com/linaspurinis/trakt.lists'
     })
     time.sleep(0.5) 
